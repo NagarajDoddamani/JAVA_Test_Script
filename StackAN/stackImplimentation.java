47d9ec30d3425c99286bcce15df17f90913b90d2
NagarajDoddamani <doddamaninagaraj5@gmail.com>
2026-02-15 23:14:29 +0530

    The test script is done all working good

--- StackAN/stackImplimentation.java
+++ StackAN/stackImplimentation.java
@@ -1,60 +1,68 @@
 package StackAN;
 
-class stackImplimentation {
+public class stackImplimentation {
     int max, top;
     int[] stack;
 
-    void stack(int max) {
+    public stackImplimentation(int max){
         this.max = max;
         this.top = -1;
         this.stack = new int[max];
     }
     
     // PUSH operation
-    void push(int x) {
+    public int push(int x) {
         if (isFull() == 1) {
-            System.out.println("Error: Stack Overflow");
+            // System.out.println("Error: Stack Overflow");
+            return 1;
         } else {
             stack[++top] = x;
-            System.out.println(x + " pushed into stack");
+            // System.out.println(x + " pushed into stack");
+            return 0;
         }
     }
 
     // POP operation
-    void pop() {
+    public int pop() {
         if (isEmpty() == 1) {
-            System.out.println("Error: Stack Underflow");
+            // System.out.println("Error: Stack Underflow");
+            return 0;
         } else {
             int x = stack[top--];
-            System.out.println("Popped element: " + x);
+            // System.out.println("Popped element: " + x);
+            return 1;
         }
     }
 
     // PEEK operation
-    void peek() {
+    public int peek() {
         if (isEmpty() == 1) {
-            System.out.println("Error: Stack is empty");
+            // System.out.println("Error: Stack is empty");
+            return 0;
         } else {
-            System.out.println("Top element: " + stack[top]);
+            // System.out.println("Top element: " + stack[top]);
+            return 1;
         }
     }
 
     // DISPLAY operation
-    void display() {
+    public int display() {
         if (isEmpty() == 1) {
-            System.out.println("Stack is empty");
+            // System.out.println("Stack is empty");
+            return 0;
         } else {
-            System.out.print("Stack elements: ");
-            for (int i = top; i >= 0; i--) {
-                System.out.print(stack[i] + " ");
-            }
-            System.out.println();
+            // System.out.print("Stack elements: ");
+            // for (int i = top; i >= 0; i--) {
+            //     System.out.print(stack[i] + " ");
+            // }
+            // System.out.println();
+            return 1;
         }
     }
 
     // isEmpty
     int isEmpty() {
-        if (top == -1)
+        if (this.top == -1)
             return 1;
         else
             return 0;
@@ -62,10 +70,18 @@ class stackImplimentation {
 
     // isFull
     int isFull() {
-        if (top == max - 1)
+        if (this.top == this.max - 1)
             return 1;
         else
             return 0;
+    }
+
+    // no random valiues 
+    public void makeFull() {
+        for (int i = 0; i < max; i++) {
+            stack[i] = i + 1; // Filling the stack with some values
+        }
+        top = max - 1; // Set top to the last index
     }
 }
 
--- testScript.java
+++ testScript.java
@@ -1,11 +1,82 @@
-import StackAN.stackImplimentation; 
+import StackAN.stackImplimentation;
 
 public class testScript {
     public static void main(String args[]){
 
         // createing the object for testing stack
-        Stack s= new Stack(10);
-
-
+        stackImplimentation s = new stackImplimentation(10);
+        
+        // if (s.isEmpty() == 1){
+        //     System.out.println("TestCase 1 : Empty st start Sucsesfull");
+        // }else{
+        //     System.out.println("TestCase 1 : Failed");
+        // }
+        // if (s.isFull() == 0){
+        //     System.out.println("TestCase 2 : Stack is not full Sucsesfull");
+        // }else{
+        //     System.out.println("TestCase 2 : isFull Failed");
+        // }
+        if (s.pop() == 0){
+            System.out.println("TestCase 1 : Stack underflow Sucsesfull");
+        }else{
+            System.out.println("TestCase 1 : Stack underflow Failed");
+        }
+        if (s.display() == 0){
+            System.out.println("TestCase 2  : Stack is empty Sucsesfull");
+        }else{
+            System.out.println("TestCase 2 : Stack is empty Failed");
+        }
+        if (s.peek() == 0){
+            System.out.println("TestCase 3 : Stack is empty Sucsesfull");
+        }else{
+            System.out.println("TestCase 3 : Stack is empty Failed");
+        }
+        if (s.push(5) == 1){
+            System.out.println("TestCase 4 : Push operation Sucsesfull");
+        }else{
+            System.out.println("TestCase 4 : Push operation Failed");
+        }
+        if (s.peek() == 1){
+            System.out.println("TestCase 5 : Peek operation Sucsesfull");
+        }else{
+            System.out.println("TestCase 5 : Peek operation Failed");
+        }
+        if (s.display() == 1){
+            System.out.println("TestCase 6 : Display operation Sucsesfull");
+        }else{
+            System.out.println("TestCase 6 : Display operation Failed");
+        }
+        if (s.pop() == 1){
+            System.out.println("TestCase 7 : Pop operation Sucsesfull");
+        }else{
+            System.out.println("TestCase 7 : Pop operation Failed");
+        }
+        if (s.pop() == 0){
+            System.out.println("TestCase 8 : Stack underflow Sucsesfull");
+        }else{
+            System.out.println("TestCase 8 : Stack underflow Failed");
+        }
+        // making full no need for intermidiate transactions
+        s.makeFull();
+        if (s.push(5) == 0){
+            System.out.println("TestCase 9 : Stack overflow Sucsesfull");
+        }else{
+            System.out.println("TestCase 9 : Stack overflow Failed");
+        }
+        if (s.display() == 0){
+            System.out.println("TestCase 10 : Display operation on full stack Failed");
+        }else{
+            System.out.println("TestCase 10 : Display operation on full stack Sucsesfull");
+        }
+        if (s.peek() == 0){
+            System.out.println("TestCase 11 : Peek operation on full stack Failed");
+        }else{
+            System.out.println("TestCase 11 : Peek operation on full stack Sucsesfull");
+        }
+        if (s.pop() == 0){
+            System.out.println("TestCase 12 : Pop operation on full stack Failed");
+        }else{
+            System.out.println("TestCase 12 : Pop operation on full stack Sucsesfull");
+        }
     }
 }
